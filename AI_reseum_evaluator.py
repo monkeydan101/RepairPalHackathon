f16468cd9f67c749c59ba598fd4b5d0ad571fd5e
Lawrence <lawrencew711kb@gmail.com>
2024-09-14 16:18:50 -0600

    yolo

--- AI_reseum_evaluator.py
+++ AI_reseum_evaluator.py
@@ -3,9 +3,12 @@
 import getpass
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-# os.environ["OPENAI_API_KEY"] = ""
+os.getenv('OPENAI_API_KEY')
 from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(model="gpt-4")
